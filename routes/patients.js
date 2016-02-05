6e211cfacf5f66f823ee2072a724f58c4652f433
purnet <teigheyyy@gmail.com>
2016-02-05 11:52:57 +1100

    initial commit

---
+++ routes/patients.js
@@ -0,0 +1,13 @@
+var state = require('./initialState');
+
+function fetchPatient (patientId) {
+  var patient = state.initialState.patients.filter((p) => {
+    if (p.id === parseInt(patientId, 10)) {
+      return p;
+    }
+  })[0];
+  
+  return patient;
+};
+
+module.exports.fetchPatient = fetchPatient;
